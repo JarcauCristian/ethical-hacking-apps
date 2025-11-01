938e824c11ddb84be6660c41ba56b2c8dc1ec426
Oprea <ionut.oprea.ext@siemens.com>
2025-11-01 21:53:47 +0200

    feature: IP and User Request Limit + Max Storage per User

--- rest-api/src/main.py
+++ rest-api/src/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import routes.get_routes
 import routes.post_routes
 import uvicorn
 import os
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from limiter_inst import limiter
 
 app = FastAPI(title="This is an API")
-app.include_router(routes.post_routes.router)
+app.state.limiter = limiter
+app.add_middleware(SlowAPIMiddleware)
 app.include_router(routes.get_routes.router)
+app.include_router(routes.post_routes.router)
 
 
 @app.get("/health")
