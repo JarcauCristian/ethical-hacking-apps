96beac5fbac8ba6f4d915c86fa618901971b0b91
10nutz <opreaionut403@ymail.com>
2025-11-10 09:20:22 +0200

    Add authentication

--- rest-api/src/main.py
+++ rest-api/src/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI, Request
-import routes.get_routes
+from fastapi.security import HTTPBearer
 import routes.post_routes
 import uvicorn
 import os
 from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
 from limiter_inst import limiter
+import auth_routes
 
-app = FastAPI(title="This is an API")
+app = FastAPI(
+    title="This is an API",
+    description="API with JWT Bearer Token Authentication",
+    version="1.0.0"
+)
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
 app.include_router(routes.get_routes.router)
 app.include_router(routes.post_routes.router)
 
+app.include_router(auth_routes.router, prefix="/auth",tags=["auth"])
 
 @app.get("/health")
 def main():
