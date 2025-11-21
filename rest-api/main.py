63bdfc6206e0118c1ac33e1fbebe3c9f6a8cc71b
Jarcau Cristian <95632351+JarcauCristian@users.noreply.github.com>
2025-11-21 20:29:09 +0200

    Update API key retrieval and app startup events
    Refactor API key handling and enhance FastAPI app configuration.

--- rest-api/main.py
+++ rest-api/main.py
@@ -12,7 +12,7 @@ from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 from limiter_inst import limiter
 import auth_routes
 
-API_KEY = "secret"
+API_KEY = os.getenv("SECRET_KEY", "supersecretkey123")
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 def require_api_key(api_key: str = Security(api_key_header)):
@@ -20,10 +20,34 @@ def require_api_key(api_key: str = Security(api_key_header)):
         raise HTTPException(status_code=403, detail="Forbidden: invalid API key")
     return api_key
 
-app = FastAPI(title="This is an API",docs_url=None,redoc_url=None,openapi_url="/openapi.json")
+app = FastAPI(
+    title="This is an API",
+    description="API with JWT Bearer Token Authentication",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url="/openapi.json"
+)
 
-app.state.limiter = limiter
+@app.on_event("startup")
+def startup_event():
+    app.state.conn = init_db()
+    app.state.limiter = limiter
+
+@app.on_event("shutdown")
+def shutdown_event():
+    app.state.conn.close()
+
 app.add_middleware(SlowAPIMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["ethapp.sedimark.work"], 
+    allow_credentials=True, 
+    allow_methods=["GET", "POST"], 
+    allow_headers=["*"], 
+)
+app.add_middleware(SecurityHeadersMiddleware)
+app.add_middleware(RequestLoggerMiddleware)
+
 app.include_router(post_routes.router)
 app.include_router(get_routes.router)
 app.include_router(auth_routes.router, prefix="/auth",tags=["auth"])
